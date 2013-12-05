56e72cb21c2a98eb5324ae9a531f143dbf85c5e7
Aymeric Gallissot <hello@aymericgallissot.fr>
2013-12-05 22:56:00 +0100

    Fix BarViewController

--- mybars/Controllers/BarViewController.m
+++ mybars/Controllers/BarViewController.m
@@ -10,14 +10,14 @@
 #import "ListViewController.h"
 #import "Bar.h"
 
-@import UIKit;
+static float const kScrollViewContentSizeHeight = 600.0;
 
 @interface BarViewController ()
 
-@property (strong, nonatomic) UIImageView *BarViewController;
 @property (nonatomic, strong) UIScrollView *scrollView;
 
 @end
+
 
 @implementation BarViewController
 
@@ -36,11 +36,6 @@
 {
     [super viewDidLoad];
     
-    
-    self.BarViewController = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 320, self.view.bounds.size.height)];
-    self.BarViewController.backgroundColor = [UIColor blackColor];
-    [self.view addSubview:self.BarViewController];
-    
     self.title = @"Bar";
     
     self.navigationController.navigationBar.barStyle = UIBarStyleBlack;
@@ -50,10 +45,16 @@
     
     self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Retour" style:UIBarButtonItemStyleDone target:self action:@selector(goBackAction)];
     
+    // ScrollView
+    self.scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0.0, 0.0, 320.0, self.view.bounds.size.height)];
+    self.scrollView.backgroundColor = [UIColor blackColor];
+    self.scrollView.contentSize = CGSizeMake(320, kScrollViewContentSizeHeight);
+    [self.view addSubview:self.scrollView];
+    
     // Image
     UIImageView *barView = [[UIImageView alloc] initWithFrame:CGRectMake(0.0, 0.0, 320.0, 267.0)];
     barView.image = [UIImage imageNamed:@"bar-1"];
-    [self.view addSubview:barView];
+    [self.scrollView addSubview:barView];
     
     // Title
     UILabel *title = [[UILabel alloc] initWithFrame:CGRectMake(15.0, 225.0, 290.0, 20.0)];
@@ -61,7 +62,7 @@
     title.font = [UIFont fontWithName:@"HelveticaNeue-Medium" size:18.0];
     title.textColor = [UIColor whiteColor];
     title.text = @"La Mécanique Ondulatoire";
-    [self.view addSubview:title];
+    [self.scrollView addSubview:title];
     
     // Address
     UILabel *address = [[UILabel alloc] initWithFrame:CGRectMake(15.0, 245.0, 290.0, 20.0)];
@@ -69,7 +70,7 @@
     address.font = [UIFont fontWithName:@"HelveticaNeue-Light" size:12.0];
     address.textColor = [UIColor whiteColor];
     address.text = @"8, Passage Thiéré";
-    [self.view addSubview:address];
+    [self.scrollView addSubview:address];
     
     // City
     UILabel *city = [[UILabel alloc] initWithFrame:CGRectMake(15.0, 259.0, 290.0, 20.0)];
@@ -77,7 +78,7 @@
     city.font = [UIFont fontWithName:@"HelveticaNeue-Light" size:12.0];
     city.textColor = [UIColor whiteColor];
     city.text = @"75011 Paris";
-    [self.view addSubview:city];
+    [self.scrollView addSubview:city];
     
     // + UIButton (details)
     UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
@@ -88,24 +89,24 @@
     [button setTitle:@"Bar / Salle de concerts" forState:UIControlStateNormal];
     [button setBackgroundImage:[UIImage imageNamed:@"button"] forState:UIControlStateNormal];
     [button setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal ];
-    [self.view addSubview:button];
+    [self.scrollView addSubview:button];
 
     UIView *lineView = [[UIView alloc] initWithFrame:CGRectMake(0.0, 305.0, self.view.bounds.size.width, 1.0)];
     lineView.backgroundColor = [UIColor colorWithRed:0.4 green:0.4 blue:0.4 alpha:0.5];
-    [self.view addSubview:lineView];
+    [self.scrollView addSubview:lineView];
     
     UIView *line = [[UIView alloc] initWithFrame:CGRectMake(71.0, 360.0, 50.0, 1.0)];
     line.backgroundColor = [UIColor colorWithRed:0.4 green:0.4 blue:0.4 alpha:0.5];
-    [self.view addSubview:line];
+    [self.scrollView addSubview:line];
     
     // Beer Icon
     UIImageView *beer = [[UIImageView alloc] initWithFrame:CGRectMake(150.0, 343.0, 25.0, 30.0)];
     beer.image = [UIImage imageNamed:@"icon-beer-big"];
-    [self.view addSubview:beer];
+    [self.scrollView addSubview:beer];
 
     UIView *line2 = [[UIView alloc] initWithFrame:CGRectMake(199.0, 360.0, 50.0, 1.0)];
     line2.backgroundColor = [UIColor colorWithRed:0.4 green:0.4 blue:0.4 alpha:0.5];
-    [self.view addSubview:line2];
+    [self.scrollView addSubview:line2];
     
 }
 
@@ -120,15 +121,5 @@
     barVC.data = self.data;
     [self.navigationController popViewControllerAnimated :YES];
 }
-
-- (void)loadView {
-    CGRect fullScreenRect=[[UIScreen mainScreen] applicationFrame];
-    UIScrollView *scrollView =[[UIScrollView alloc] initWithFrame:fullScreenRect];
-    scrollView.contentSize=CGSizeMake(320,480);
-    scrollView.contentInset=UIEdgeInsetsMake(64.0,0.0,44.0,0.0);
-    //scrollView.scrollIndicatorInsets=UIEdgeInsetsMake(64.0,0.0,44.0,0.0);
-    self.view=scrollView;
-}
-
 
 @end
